f7d1b889ccaa5b0fdc4f0efd28d0dbe3799963d1
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-12-30 09:19:52 -0600

    Create owners and cats tables

--- server/models.py
+++ server/models.py
@@ -1,6 +1,33 @@
+
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
 # Models go here!
+
+class Owner(db.Model, SerializerMixin):
+    __tablename__ = "owners"
+
+    serialize_rules = ("-cats.owner", )
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Relationships
+    cats = db.relationship("Cat", back_populates="owner")
+
+    pass
+
+class Cat(db.Model, SerializerMixin):
+    __tablename__ = "cats"
+
+    serialize_rules = ("-owner.cats", )
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Relationships
+    owner_id = db.Column(db.Integer, db.ForeignKey("owners.id"))
+    owner = db.relationship("Owner", back_populates="cats")
+
+    pass
